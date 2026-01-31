9877d4f09e170258d445de81e164d738d3bc86c7
IngOscar19 <l.eiro040919@itses.edu.mx>
2026-01-31 17:04:03 -0600

    Ajustes y correcion de estilo en el carrito

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,37 +1,33 @@
 import React from 'react'
+import Item from './item.jsx'
 
 export default function Header({ cart, setShowCart }) {
   return (
     <header className="py-5 header">
       <div className="container-xl">
         <div className="row justify-content-center justify-content-md-between">
-
+          
           <div className="col-8 col-md-3">
             <a href="/">
-              <img
-                className="img-fluid"
-                src="/img/logo.svg"
-                alt="imagen logo"
-              />
+              <img className="img-fluid" src="/img/logo.svg" alt="imagen logo" />
             </a>
           </div>
-          <nav className="col-md-6 mt-5 d-flex align-items-start justify-content-end position-relative">
-            <button
-              className="btn p-0 border-0 bg-transparent"
-              onClick={() => setShowCart(prev => !prev)}
-            >
-              <img
-                className="img-fluid"
-                src="/img/carrito.png"
-                alt="imagen carrito"
-              />
-            </button>
 
-            {cart.length > 0 && (
-              <span className="badge bg-danger position-absolute top-0 start-100 translate-middle">
-                {cart.length}
-              </span>
-            )}
+          <nav className="col-md-6 mt-5 d-flex align-items-start justify-content-end">
+            
+            <div className="carrito">
+                
+
+                <img 
+                  className="img-fluid" 
+                  src="/img/carrito.png" 
+                  alt="imagen carrito" 
+                />
+                <div id="carrito" className="bg-white p-3">
+                    
+                    <Item cart={cart} />
+                </div>
+            </div>
           </nav>
 
         </div>
